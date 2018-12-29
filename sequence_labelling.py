6be3a78578b3aacb6cee89e9107a758f34c48e81
lc-chen <chen@exabel.com>
2018-12-29 20:40:40 +0100

    add acme data for ner

--- sequence_labelling.py
+++ sequence_labelling.py
@@ -23,6 +23,29 @@ import requests
 from bs4 import BeautifulSoup as bs
 from bs4.element import Tag
 
+
+def get_text_and_annotations_from_file(dataset_folder: Path):
+    ner_texts = []
+    ner_annotations = []
+    unsorted_annotation_files = dataset_folder.glob('*.annotation.json')
+    unsorted_text_files = dataset_folder.glob('*.text.json')
+    annotation_files = sorted([x for x in unsorted_annotation_files])
+    text_files = sorted([x for x in unsorted_text_files])
+    for text_file, annotation_file in zip(text_files, annotation_files):
+        assert text_file.name.split('.')[0] == annotation_file.name.split('.')[0]
+        textf = dataset_folder.joinpath(text_file)
+        annotationf = dataset_folder.joinpath(annotation_file)
+        with textf.open('r') as tf, annotationf.open('r') as af:
+            for i, (line1, line2) in enumerate(zip(tf, af)):
+                # if i > 100:
+                #     break
+                text = json.loads(line1)
+                annotations = json.loads(line2)
+                ner_texts.append(text)
+                ner_annotations.append(annotations)
+    return ner_texts, ner_annotations
+
+
 def _download_reuters():
     """
     Download Stanford Sentiment Treebank to enso `data` directory
@@ -62,6 +85,7 @@ def _download_reuters():
     with open(processed_path, 'wt') as fp:
         json.dump((docs, docs_labels), fp)
 
+
 n_sample = 100
 n_hidden = 768
 dataset_path = os.path.join(
@@ -69,25 +93,33 @@ dataset_path = os.path.join(
 )
 processed_path = os.path.join('/media/liah/DATA/reuters', 'reuters.json')
 
-_download_reuters()
+# _download_reuters()
 
-with open(processed_path, 'rt') as fp:
-    texts, labels = json.load(fp)
+# with open(processed_path, 'rt') as fp:
+#     texts, labels = json.load(fp)
 
 tf.reset_default_graph()
 
-model = SequenceLabeler(batch_size=2, max_length=256, lm_loss_coef=0.0, verbose=False)
+model = SequenceLabeler(batch_size=32, max_length=256, lm_loss_coef=0.0, verbose=False)
 
-raw_docs = ["".join(text) for text in texts]
+# raw_docs = ["".join(text) for text in texts]
 # raw_docs : list of article str
 # texts : is raw_docs
-# annotations : list of list of dict, each dict is a {start, end, label, text} of an NE, each article contains several
-#               such dict
+# annotations : list of list of dict, each dict is a {char_start, char_end, label, text} of an NE, each article
+#               contains several such dict
+# texts, annotations = finetune_to_indico_sequence(raw_docs, texts, labels)
 
-texts, annotations = finetune_to_indico_sequence(raw_docs, texts, labels)
-train_texts, test_texts, train_annotations, test_annotations = train_test_split(texts, annotations, test_size=0.1)
-# model.fit(train_texts)
+dataset_folder: Path = Path('/media/liah/DATA/acme_data_ner/dataset_ner_en')
+texts, annotations = get_text_and_annotations_from_file(dataset_folder=dataset_folder)
+data_size = len(texts)
+train_texts, test_texts, train_annotations, test_annotations = train_test_split(texts[:data_size],
+                                                                                annotations[:data_size],
+                                                                                test_size=0.1)
+
+# model.fit(train_texts) # unsup. learning for lm
 model.fit(train_texts, train_annotations)
 predictions = model.predict(test_texts)
 probas = model.predict_proba(test_texts)
 print(probas)
+model_store_path = '/media/liah/DATA/acme_data_ner/models/model{:d}'.format(data_size)
+model.save(model_store_path)
